3318e7d4e1b07bc41c2893b3a24b396e29d7b71b
Clotaire Douziech <clotairedouziech@gmail.com>
2026-06-15 23:27:18 +0200

    feat: added title and buttons part - refs #12

--- frontend/src/pages/note/Note.tsx
+++ frontend/src/pages/note/Note.tsx
@@ -9,11 +9,21 @@ const Note = () => {
 
   return (
     <div className="flex h-screen">
-      <Sidebar />
-      <div className="note-container flex justify-center items-center gap-4 w-full">
-        <NoteContentContainer value={content} onChange={setContent} />
-        <NoteVueContainer content={content} />
-      </div>
+        <Sidebar />
+        <div className="flex flex-col p-4 w-full">
+            <textarea name="title" id="title" placeholder="Note Title" className="bg-transparent border border-gray-600 text-gray-300 placeholder:text-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500"></textarea>
+            <div className="note-container flex items-center gap-4 ">
+                <NoteContentContainer value={content} onChange={setContent} />
+                <NoteVueContainer content={content} />
+                        <div>
+            <button>Save Note</button>
+            <button>Delete Note</button>
+        </div> 
+            </div>
+        </div>
+
+    
+
     </div>
   )
 }

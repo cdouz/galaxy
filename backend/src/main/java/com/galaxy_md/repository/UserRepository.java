311214b402a38bf55665b32c27273daeeb3dd54b
Clotaire Douziech <clotairedouziech@gmail.com>
2026-08-17 01:02:31 +0200

    feat: add User entity and UserRepository

---
+++ backend/src/main/java/com/galaxy_md/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.galaxy_md.repository;
+
+import com.galaxy_md.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+}

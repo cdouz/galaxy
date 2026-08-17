309a87e8d3941f532819656691fea9713285e6f8
Clotaire Douziech <clotairedouziech@gmail.com>
2026-08-18 00:11:23 +0200

    quick fix

--- backend/src/main/java/com/galaxy_md/security/RestAuthenticationEntryPoint.java
+++ backend/src/main/java/com/galaxy_md/security/RestAuthenticationEntryPoint.java
@@ -1,6 +1,6 @@
 package com.galaxy_md.security;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.ObjectMapper;
 import com.galaxy_md.exception.ErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
